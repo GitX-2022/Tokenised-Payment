4a58e19048b69e7cf2d871ad69a0a4ed20f96dee
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2022-10-02 07:16:15 +0530

    Added Solidity File

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
       });
     setAccounts(accounts);
     console.log(accounts);
+
   }
 
   const checkBalance = async (e) => {
@@ -68,64 +69,52 @@ function App() {
 
   const sendTransaction = async (e) => {
     e.preventDefault();
-    const provider = await detectEthereumProvider();
-
-    if (!provider) {
-      console.log("No provider detected");
-      return;
-    } else {
-      console.log(provider);
-    }
-
-    let Web3Client = new Web3(provider);
 
     let tokenAddress = "0xE3E8b36dCEA6ABa09cAdca6Cb06724D6dC9C5E1d";
     let toAddress = "0x2C154049dF236a281fc1BF400564c60F2FcA2042";
-    let fromAddress = accounts[0];
 
-    let decimals = Web3.utils.toBN(0);
-    let amount = Web3.utils.toBN(20);
+    function getDataFieldValue(tokenRecipientAddress, tokenAmount) {
+      const web3 = new Web3();
+      const TRANSFER_FUNCTION_ABI = {
+        constant: false,
+        inputs: [
+          { name: "_to", type: "address" },
+          { name: "_value", type: "uint256" },
+        ],
+        name: "transfer",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+      };
+      return web3.eth.abi.encodeFunctionCall(TRANSFER_FUNCTION_ABI, [
+        tokenRecipientAddress,
+        tokenAmount,
+      ]);
+    }
 
-    let minABI = [
-      {
-        "constant": false,
-        "inputs": [
+    await window.ethereum
+      .request({
+        method: "eth_sendTransaction",
+        params: [
           {
-            "name": "_to",
-            "type": "address"
+            from: accounts[0],
+            to: tokenAddress,
+            data: getDataFieldValue(toAddress, 5),
           },
-          {
-            "name": "_value",
-            "type": "uint256"
-          }
         ],
-        "name": "transfer",
-        "outputs": [
-          {
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "type": "function"
-      }
-    ];
-
-    let contract = new Web3Client.eth.Contract(minABI, tokenAddress, { from: fromAddress });
-
-    let value = 25;
-
-    let a = contract.methods
-      .transfer(toAddress, value).call()
-      .then((res) => {
-        console.log(res);
-        // setBalance(res);
       })
-      .catch((err) => console.log(err));;
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
   }
 
   return (
     <div className="App">
       <h1>Intra-college Tokenised Payment System</h1>
+
+      <p>Connected with {accounts} account</p>
+      <p>Balance: {balance} PSG Coins</p>
+
       <button onClick={(e) => connectWallet(e)}>Connect wallet</button>
       <button onClick={(e) => checkBalance(e)}>Check Balance</button>
       <button onClick={(e) => sendTransaction(e)}>Send Transaction</button>
